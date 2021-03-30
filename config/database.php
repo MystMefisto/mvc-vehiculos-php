5f7fd1007a05c524df5da3c4d7a94a447066dd7c
MystMefisto <carlosteiker@gmail.com>
2021-03-30 17:06:00 -0600

    configure db

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@
     class Conectar{
 
         public static function conexion(){
-            $conexion = new mysqli("localhost","root","","mvc php");
+            $conexion = new mysqli("us-cdbr-east-03.cleardb.com:3306","b36ace11129452","16c987a7","heroku_2d2a0ab3712a880");
             return $conexion;
         }
     } 
